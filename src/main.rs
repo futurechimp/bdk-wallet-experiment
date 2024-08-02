4a2fbedc0dd8950a5d65919ec1ee9188612bb2d2
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2024-08-02 11:37:41 +0100

    The simplest possible descriptor and spend I can think of

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,178 @@
-use std::{collections::BTreeMap, str::FromStr};
+use bdk_wallet::{bitcoin::absolute::LockTime, miniscript::psbt::PsbtInputExt};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    io::Write,
+    str::FromStr,
+    time::Duration,
+};
+use tokio::time::sleep;
 
+use bdk_esplora::{esplora_client, EsploraAsyncExt};
 #[allow(unused)]
 use bdk_wallet::bitcoin::{Amount, Network};
 use bdk_wallet::{
-    bitcoin::{self, sighash::SighashCache},
+    bip39::Mnemonic,
+    bitcoin::{self, bip32::Xpub, key::Secp256k1, sighash::SighashCache, PublicKey},
+    chain::Persisted,
+    keys::{DerivableKey, ExtendedKey},
     miniscript::{psbt::PsbtExt, Descriptor},
+    rusqlite,
+    template::Bip84,
+    KeychainKind, SignOptions, Wallet,
 };
 use bdk_wallet::{
-    bitcoin::{
-        psbt, secp256k1, transaction, OutPoint, Psbt, Script, Sequence, Transaction, TxIn, TxOut,
-    },
-    miniscript::psbt::PsbtInputExt,
+    bitcoin::{psbt, transaction, OutPoint, Psbt, Script, Transaction, TxIn, TxOut},
+    miniscript::policy::Concrete,
 };
 
-// These constants can be adjusted to control program flow as desired.
-const _AFTER: u32 = 5; // ~2 minutes when using mutinynet.com
-const BRIDGE_FUNDS: bool = false; // change this to true when you want to fund the vault
-const UNBRIDGE_FUNDS: bool = true; // switch between unvault and emergency usage
-
-// You shouldn't really need to touch these.
 const DB_PATH: &str = "bdk-wallet.sqlite";
-const NETWORK: Network = Network::Signet;
 const ESPLORA_URL: &str = "https://mutinynet.com/api";
+const STOP_GAP: usize = 5;
+const PARALLEL_REQUESTS: usize = 5;
 
-mod esplora;
-mod keys;
-mod utils;
+// A pretty standard sync method, shouldn't be anything controversial here.
+pub(crate) async fn sync(
+    client: &esplora_client::AsyncClient,
+    wallet: &mut Persisted<Wallet>,
+    conn: &mut rusqlite::Connection,
+) -> anyhow::Result<()> {
+    println!("Syncing...");
 
-// A port of the vault idea from https://bitcoinerlab.com/guides/miniscript-vault to Rust. Assume we have two users:
-//
-// * Alice worries a lot about having her keys stolen. She can "unvault" her funds only after the expiration of a timelock.
-// * Bob is a Buddhist saint living on a mountain top in Tibet. He can move the locked funds at any time when he gets a phone call from Alice.
-//
-// We want to use existing keys.
-//
-// Alice plays the role of the `unvault_key` user. She will keep her funds in the vault. She can't unlock funds until vault time expires.
-//
-// Alice's good buddy Bob, on the other hand, will be the incorruptible and well-protected Tibetan monk who holds the `emergency_key`.
+    let request = wallet.start_full_scan().inspect_spks_for_all_keychains({
+        let mut once = BTreeSet::<KeychainKind>::new();
+        move |keychain, spk_i, _| {
+            if once.insert(keychain) {
+                print!("\nScanning keychain [{:?}] ", keychain);
+            }
+            print!(" {:<3}", spk_i);
+            std::io::stdout().flush().expect("must flush")
+        }
+    });
+
+    let mut update = client
+        .full_scan(request, STOP_GAP, PARALLEL_REQUESTS)
+        .await?;
+    let now = std::time::UNIX_EPOCH.elapsed().unwrap().as_secs();
+    let _ = update.graph_update.update_last_seen_unconfirmed(now);
+
+    wallet.apply_update(update)?;
+    wallet.persist(conn)?;
+
+    Ok(())
+}
+
+// Find the Outpoint by spk, useful for ensuring that we grab the right
+// output transaction to use as input for our spend transaction
+fn get_vout(tx: &Transaction, spk: &Script) -> (OutPoint, TxOut) {
+    for (i, txout) in tx.clone().output.into_iter().enumerate() {
+        if spk == &txout.script_pubkey {
+            return (OutPoint::new(tx.compute_txid(), i as u32), txout);
+        }
+    }
+    panic!("Only call get vout on functions which have the expected outpoint");
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    utils::tracing_setup();
+    let amount = 1000;
 
-    let amount = 10000;
+    // create a wallet
+    let words = "property blush sun knock heavy animal lens syrup matrix february lava chalk";
+    let mnemonic = Mnemonic::parse(words).unwrap();
+    let xkey: ExtendedKey = mnemonic
+        .clone()
+        .into_extended_key()
+        .expect("couldn't turn mnemonic into xkey");
+    let xprv = xkey
+        .into_xprv(Network::Signet)
+        .expect("unable to turn xkey into xpriv");
 
-    // We  have two identities, `alice` and `bob`. Let's assume they have wallets that they're already using, rather than
-    // hard-coding policy strings.
+    // Re-generate the public key, this is a bit gross but it's necessary as xkey
+    // gets consumed on use.
+    let xkey: ExtendedKey = mnemonic
+        .into_extended_key()
+        .expect("couldn't turn mnemonic into xkey");
 
-    // Alice will hold the master private key
-    let mut alice = esplora::Client::new(
-        "alice",
-        "property blush sun knock heavy animal lens syrup matrix february lava chalk",
-        NETWORK,
-    )?;
+    let secp = Secp256k1::new();
+    let xpub: Xpub = xkey
+        .into_extended_key()
+        .expect("couldn't turn mnemonic into xkey")
+        .into_xpub(Network::Signet, &secp);
 
-    // Bob holds backup1
-    let bob = esplora::Client::new(
-        "bob",
-        "shuffle security crazy source shaft nerve improve bone estate grit brain gold",
-        NETWORK,
-    )?;
+    // Create an Esplora client
+    let client = esplora_client::Builder::new(ESPLORA_URL).build_async()?;
+    let mut conn = rusqlite::Connection::open(DB_PATH)?;
 
-    // Carol holds backup2
-    let carol = esplora::Client::new(
-        "carol",
-        "advance simple kitchen monitor twice rescue crunch you party clerk screen inherit",
-        NETWORK,
-    )?;
+    let external_descriptor = Bip84(xprv.clone(), KeychainKind::External);
+    let internal_descriptor = Bip84(xprv.clone(), KeychainKind::Internal);
+    let mut wallet = Wallet::create(external_descriptor, internal_descriptor)
+        .network(Network::Signet)
+        .create_wallet(&mut conn)?;
 
-    // Dave holds backup2
-    let dave = esplora::Client::new(
-        "dave",
-        "uniform you loop talk orchard polar issue chronic priority garbage actress cradle",
-        NETWORK,
-    )?;
+    // Sync the client and wallet
+    sync(&client, &mut wallet, &mut conn)
+        .await
+        .expect("client sync failed");
 
-    alice.sync().await?;
-    alice.balance();
-
-    let secp256k1 = secp256k1::Secp256k1::new();
-
-    let alice_pk = alice.wallet_public_key.to_string();
-    let bob_pkh = bob
-        .wallet_public_key
-        // .to_pubkeyhash(SigType::Ecdsa)
-        .to_string();
-    let carol_pkh = carol
-        .wallet_public_key
-        // .to_pubkeyhash(SigType::Ecdsa)
-        .to_string();
-    let dave_pkh = dave
-        .wallet_public_key
-        // .to_pubkeyhash(SigType::Ecdsa)
-        .to_string();
-
+    // Get Alice's public key so we can write it into the policy/descriptor
+    let alice_pk = xpub.public_key;
     println!("alice: {}", alice_pk);
-    println!("bob: {}", bob_pkh);
 
-    let s = format!("wsh(t:or_c(pk({alice_pk}),v:thresh(1,pkh({bob_pkh}),a:pkh({carol_pkh}),a:pkh({dave_pkh}))))#0y76yhr7");
-    let bridge_descriptor = Descriptor::from_str(&s).unwrap();
+    let policy_str = format!("pk({alice_pk})");
+    let policy = Concrete::<PublicKey>::from_str(&policy_str)?;
+    println!("{}", policy);
+    let descriptor =
+        Descriptor::new_wsh(policy.compile().unwrap()).expect("could not create descriptor");
 
-    assert!(bridge_descriptor.sanity_check().is_ok());
+    assert!(descriptor.sanity_check().is_ok());
+    println!("Descriptor pubkey script: {:?}", descriptor.script_pubkey());
     println!(
-        "Bridge pubkey script: {}",
-        bridge_descriptor.script_pubkey()
-    );
-    println!(
-        "Bridge address: {}",
-        bridge_descriptor.address(Network::Signet).unwrap()
-    );
-    println!(
-        "Weight for witness satisfaction cost {}",
-        bridge_descriptor.max_weight_to_satisfy().unwrap()
+        "Descriptor address: {}",
+        descriptor.address(Network::Signet).unwrap()
     );
 
-    // Transfer some funds into the bridge
-    let deposit_psbt = alice.simple_transfer(
-        bridge_descriptor.address(Network::Signet)?,
-        Amount::from_sat(amount),
-    )?;
+    let mut tx_builder = wallet.build_tx();
+    tx_builder
+        .add_recipient(descriptor.script_pubkey(), Amount::from_sat(amount))
+        .enable_rbf();
+    let mut deposit_psbt = tx_builder.finish()?;
+    let finalized = wallet.sign(&mut deposit_psbt, SignOptions::default())?;
+    assert!(finalized);
+
+    // Extract the transaction from the deposit psbt
     let deposit_tx = deposit_psbt.extract_tx()?;
 
-    if BRIDGE_FUNDS {
-        alice.broadcast(&deposit_tx).await?;
-    }
+    // Broadcast the transaction
+    client.broadcast(&deposit_tx).await?;
 
     println!(
-        "depo tx may be at: https://mutinynet.com/tx/{}",
+        "depo tx is at: https://mutinynet.com/tx/{}",
         deposit_tx.compute_txid()
     );
 
+    // Sleep for 30 seconds to ensure Mutiny mines a block - it has a 30 second block time.
+    sleep(Duration::from_secs(30)).await;
+
+    // At this point, we've got a very simple policy `pk({alice_pk})`, into which we've inserted
+    // Alice's public key, turned into a descriptor, and generated an address. We have sent sats
+    // into the generated address. How do we transfer the sats back out?
+    //
+    // My current, perhaps wrong, understanding, is that we need to manually generate a
+    // bitcoin::Transaction with UTXOs based on the hex of the deposit_tx transaction
+    // we just sent.
+
+    // The only place I have seen code which attempts to spend locked funds is at
+    // https://github.com/rust-bitcoin/rust-miniscript/blob/master/examples/psbt_sign_finalize.rs
+    // so the code below is largely copied from there. I have tried to boil it down to the simplest
+    // possible descriptor.
+
     let spend_tx = Transaction {
         version: transaction::Version::TWO,
-        lock_time: bitcoin::absolute::LockTime::from_consensus(50),
+        lock_time: LockTime::ZERO,
         input: vec![],
         output: vec![],
     };
 
-    // Spend one input and spend one output for simplicity.
     let mut psbt = Psbt {
         unsigned_tx: spend_tx,
         unknown: BTreeMap::new(),
@@ -147,30 +183,29 @@ async fn main() -> anyhow::Result<()> {
         outputs: vec![],
     };
 
-    let (outpoint, witness_utxo) = get_vout(&deposit_tx, &bridge_descriptor.script_pubkey());
+    let (outpoint, witness_utxo) = get_vout(&deposit_tx, &descriptor.script_pubkey());
 
     let txin = TxIn {
         previous_output: outpoint,
-        sequence: Sequence::from_height(26),
         ..Default::default()
     };
     psbt.unsigned_tx.input.push(txin);
 
     psbt.unsigned_tx.output.push(TxOut {
-        script_pubkey: alice.next_unused_address()?.script_pubkey(),
-        value: Amount::from_sat(amount / 5 - 500),
-    });
-
-    psbt.unsigned_tx.output.push(TxOut {
-        script_pubkey: bridge_descriptor.script_pubkey(),
-        value: Amount::from_sat(amount * 4 / 5),
+        script_pubkey: wallet
+            .next_unused_address(KeychainKind::External)
+            .script_pubkey(),
+        value: Amount::from_sat(amount - 250),
     });
 
     // Generating signatures & witness data
     let mut input = psbt::Input::default();
-    input
-        .update_with_descriptor_unchecked(&bridge_descriptor)
-        .unwrap();
+
+    // Killer problem: uncommenting the input update causes compile-time error.
+    // The psbt input wants a Descriptor<DefiniteDescriptorKey> rather than
+    // a Descriptor<PublicKey>. However switching descriptor types gets other errors I'm not sure
+    // how to code around.
+    // input.update_with_descriptor_unchecked(&descriptor).unwrap();
 
     input.witness_utxo = Some(witness_utxo.clone());
     psbt.inputs.push(input);
@@ -186,27 +221,11 @@ async fn main() -> anyhow::Result<()> {
     // Fixme: Take a parameter
     let hash_ty = bitcoin::sighash::EcdsaSighashType::All;
 
-    // let sk1 = bob.wallet_private_key;
-    // let sk2 = carol.wallet_private_key;
-
-    // Finally construct the signature and add to psbt
-    // let sig1 = secp256k1.sign_ecdsa(&msg, &sk1);
-    // let pk1: bitcoin::PublicKey = sk1.public_key(&secp256k1).into();
-
-    // assert!(secp256k1
-    //     .verify_ecdsa(&msg, &sig1, &sk1.public_key(&secp256k1))
-    //     .is_ok());
-
-    // Second key just in case
-    // let sig2 = secp256k1.sign_ecdsa(&msg, &sk2);
-    // let pk2 = sk2.public_key(&secp256k1);
-    // assert!(secp256k1.verify_ecdsa(&msg, &sig2, &pk2).is_ok());
-
     // Since the above is not working, let's try signing with the alice key
-    let alice_sig = secp256k1.sign_ecdsa(&msg, &alice.wallet_private_key);
+    let alice_sig = secp.sign_ecdsa(&msg, &xprv.private_key);
 
     psbt.inputs[0].partial_sigs.insert(
-        alice.wallet_public_key.into(),
+        xpub.public_key.into(),
         bitcoin::ecdsa::Signature {
             signature: alice_sig,
             sighash_type: hash_ty,
@@ -216,30 +235,14 @@ async fn main() -> anyhow::Result<()> {
     // println!("{:#?}", psbt);
     // println!("{}", psbt);
 
-    psbt.finalize_mut(&secp256k1).unwrap();
+    psbt.finalize_mut(&secp).unwrap();
 
     // println!("finalize: {:#?}", psbt);
 
-    let unbridge_tx = psbt.extract_tx().expect("failed to extract tx");
-    // println!("{}", bitcoin::consensus::encode::serialize_hex(&unbridge_tx));
+    let spend_tx = psbt.extract_tx().expect("failed to extract tx");
+    // println!("{}", bitcoin::consensus::encode::serialize_hex(&spend_tx));
 
-    // This constant is set up at the top of the file
-    if UNBRIDGE_FUNDS {
-        alice.broadcast(&unbridge_tx).await?;
-    } else {
-        // Bob is going to try and immediately use his emergency key and transfer
-        // I assume I need to create a wallet here using the `vault_descriptor`?
-    }
+    client.broadcast(&spend_tx).await?;
 
     Ok(())
-}
-
-// Find the Outpoint by spk
-fn get_vout(tx: &Transaction, spk: &Script) -> (OutPoint, TxOut) {
-    for (i, txout) in tx.clone().output.into_iter().enumerate() {
-        if spk == &txout.script_pubkey {
-            return (OutPoint::new(tx.compute_txid(), i as u32), txout);
-        }
-    }
-    panic!("Only call get vout on functions which have the expected outpoint");
 }
